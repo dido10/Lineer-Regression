f44527647b6457ed2a9ff7353bad0096e51dff6d
dido10 <didodurmaz@hotmail.com>
2019-05-20 11:33:19 +0300

    Update multiplelinearreg.py

--- multiplelinearreg.py
+++ multiplelinearreg.py
@@ -1,18 +1,23 @@
 
-#multiple linear regression y=b0+b1*x1+b2*x2
-import pandas as pd
-import numpy as np
+#multiple linear regression y=b0+b1*x1+b2*x2import pandas as pd
+import numpy as mp
 from sklearn.linear_model import LinearRegression
-df=pd.read_csv("regressiondataset.csv",sep=";")
-y=df.maas.values.reshape(-1,1)
+
+df=pd.read_csv("multiple-linear-regression-dataset.csv",sep=";")
+
 x=df.iloc[:,[0,2]].values
+y=df.maas.values.reshape(-1,1)
 
-multiplereg=LinearRegression()
-multiplereg.fit(x,y)
+#%% Predict
 
-print("b0:",multiplereg.intercept_)
-print("b1 and b2 :",multiplereg.coef_)
+multiple_linear_reg=LinearRegression()
+multiple_linear_reg.fit(x,y)
 
+print("bo:", multiple_linear_reg.intercept_) #x=0 için ilk değer
+print("b1,b2", multiple_linear_reg.coef_) 
 
-multiplereg.predict(np.array([[2,27],[15,48]]))
+#A=[[2.5,28]] bu şekilde predict için uygun hale geliyor.
+multiple_linear_reg.predict([[2.5,28]])
 
+#♥ bunu nasıl yaptım acaba?? :D
+multiple_linear_reg.predict([[2.5,28],[5,30]])
